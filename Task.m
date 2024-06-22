de58d1422058cb7d58ce0d8da1dc49d0726f9d93
Jack Halloween <78194804+jackhallloween21@users.noreply.github.com>
2024-06-22 20:38:10 +0530

    Create Task.m

---
+++ Task.m
@@ -0,0 +1,16 @@
+Overview:
+The theme for this week’s bounty is personal productivity.
+Required Tools and Resources
+Composio Platform: Access our documentation here for a comprehensive guide: Composio Overview.
+Gemini/OpenAI API: Follow this guide to obtain the API key: API Key Guide.
+Agentic Frameworks: Choose from Crewai, Langchain, or Llamaindex to build your agent. Refer to the respective documentation for guidance.
+
+Judging Criteria:
+Attention to Detail: How well does the implementation adhere to the project specifications and user needs?
+Usability: Is the application intuitive and user-friendly?
+Code Quality: Clarity, maintainability, and adherence to standard programming practices.
+Contest Tasks:
+Getting Started:
+Initial Setup: Star our open-source GitHub repository using a program developed with Composio by creating functionality that stars a repo based on user input.
+Main Assignment:
+Build an Agent: Develop an agent that connects Discord to Google Calendar, enabling management of Google Calendar activities through Discord using Composio.
